7233320aeac290d2f276d0a5a2a4e3f5c754465f
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-04-09 16:11:30 +0545

    datePicker

--- project 4/ContentView.swift
+++ project 4/ContentView.swift
@@ -9,15 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
     
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4 ... 12, step: 0.25){
-            Text("\(sleepAmount, specifier: "%g") hours")
+        Form {
+            let now = Date()
+            let tomorrow = Date().addingTimeInterval(86400)
+            let range = now ... tomorrow
+            
+            DatePicker("Please Enter a Date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+        
         }
     }
-}
 
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
