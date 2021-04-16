55fd756443d8a34031cae858c4067bc8b371f40f
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-04-16 15:10:06 +0545

    tutorial 5

--- project 4/ContentView.swift
+++ project 4/ContentView.swift
@@ -22,9 +22,39 @@ struct ContentView: View {
                 DatePicker("Please enter your time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
                 
-                //more to come
+                Text("Desired amount to sleep")
+                    .font(.headline)
+                
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g")hours")
+                }
+                
+                Text("Dailly coffee intake")
+                    .font(.headline)
+                
+                Stepper(value: $coffeeAmount, in: 1...20) {
+                    if coffeeAmount == 1 {
+                        Text("One cup")
+                    }else {
+                        Text("\(coffeeAmount)cups")
+                    }
+                }
+                
+                
             }
+            .navigationTitle("BetterRest")
+            .navigationBarItems(trailing:
+//                                    here we have used calculateBedtime() without patenthesis because thaat means that call the function when button is tapped and the function returns a closute to run
+//                                    but while only writing the name without patenthesis means run the function when button is tappes without expecting return or anything
+                Button (action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
         }
+        
+    }
+    
+    func calculateBedtime() {
         
     }
 
