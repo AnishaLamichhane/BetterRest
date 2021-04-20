dd7cf8433dd73ab6172bbc4ed65cd40778fd5967
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-04-20 17:47:04 +0545

    ContentView.swift

--- project 4/ContentView.swift
+++ project 4/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    @State private var alertTitle = ""
+//    @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
@@ -41,30 +41,49 @@ struct ContentView: View {
                     Text("Dailly coffee intake")
                         .font(.headline)
                     
-                    Stepper(value: $coffeeAmount, in: 1...20) {
-                        if coffeeAmount == 1 {
-                            Text("One cup")
-                        }else {
-                            Text("\(coffeeAmount)cups")
+//                    Stepper(value: $coffeeAmount, in: 1...20) {
+//                        if coffeeAmount == 1 {
+//                            Text("One cup")
+//                        }else {
+//                            Text("\(coffeeAmount)cups")
+//                        }
+//                    }
+                    
+                    //Challenge 2
+                    Picker("", selection: $coffeeAmount) {
+                        ForEach( 1 ..< 20, id: \.self) { coffeeAmount in
+                            Text("\(coffeeAmount) \(coffeeAmount == 1 ? "cup" : "cups" ) ")
                         }
                     }
+                    .labelsHidden()
+                }
+                
+                Section(header: Text("Recommended bed time").font(.headline)) {
+                    Text("\(calculateBedtime)")
+                        .font(.title)
+                   
+                    
                 }
                 
             }
             .navigationTitle("BetterRest")
-            .navigationBarItems(trailing:
+            
+            
+           // .navigationBarItems(trailing: )
 //  here we have used calculateBedtime() without patenthesis because thaat means that call the function when button is tapped and the function returns a closute to run
 //   but while only writing the name without patenthesis means run the function when button is tappes without expecting return or anything
-                Button (action: calculateBedtime) {
-                    Text("Calculate")
-                }
-            )
-            .alert(isPresented: $showingAlert){
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            }
+//                Button (action: calculateBedtime) {
+//                    Text("Calculate")
+//                }
+//            )
+//            .alert(isPresented: $showingAlert){
+//                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+//            }
         }
         
     }
+    
+    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
@@ -72,27 +91,27 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date()
     }
     
-    func calculateBedtime() {
+    var calculateBedtime: String {
         let model = SleepCalculator()
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
         let minute = (components.minute ?? 0) * 60
-        
-        do {
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: Double(sleepAmount), coffee: Double(coffeeAmount ))
-            let sleepTime = wakeUp - prediction.actualSleep
-            
-            let formatter = DateFormatter()
-            formatter.timeStyle = .short
-            
-            alertMessage = formatter.string(from: sleepTime)
-            alertTitle = "Your ideal bedtime is ..."
-        }
-        catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry there is a problem calculating your bedtime"
-        }
-        showingAlert = true
+    
+        var message: String
+            do {
+                let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: Double(sleepAmount), coffee: Double(coffeeAmount ))
+                let sleepTime = wakeUp - prediction.actualSleep
+                
+                let formatter = DateFormatter()
+                formatter.timeStyle = .short
+                message = formatter.string(from: sleepTime)
+         
+            }
+            catch {
+                
+                message = "Sorry there is a problem calculating your bedtime"
+            }
+            return message
     }
 
 }
